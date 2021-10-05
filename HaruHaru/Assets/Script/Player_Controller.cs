11921c19eb00e3d665faba4bbdc5a2acdef87916
Hibyeol <63093685+Hibyeol@users.noreply.github.com>
2021-10-05 10:28:59 +0900

    2021.10.05
    라운드 구현  완료

--- HaruHaru/Assets/Script/Player_Controller.cs
+++ HaruHaru/Assets/Script/Player_Controller.cs
@@ -10,6 +10,7 @@ public class Player_Controller : MonoBehaviour
     private Transform cameraArm;
 
     Animator animator;
+    public float health;
 
     // Start is called before the first frame update
     void Start()
@@ -36,8 +37,8 @@ public class Player_Controller : MonoBehaviour
             Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
             Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
 
-            playerBody.forward = lookForward;
-            transform.position += moveDir * Time.deltaTime * 2f;
+            playerBody.forward = lookForward; // 바라보는 방향
+            transform.position += moveDir * Time.deltaTime * 2f; // 이동
         }
         Debug.DrawRay(cameraArm.position, new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized, Color.red);
     }
@@ -50,13 +51,28 @@ public class Player_Controller : MonoBehaviour
 
         if(x < 180f)
         {
-            x = Mathf.Clamp(x, -1f, 70f);
+            x = Mathf.Clamp(x, -1f, 70f);//카메라 각도 제한
         }
         else
         {
-            x = Mathf.Clamp(x, 335f, 361f);
+            x = Mathf.Clamp(x, 335f, 361f); // 카메라 각도 제한
         }
 
-        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z); // 카메라 각도
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (other.tag == "Tomato")
+        {
+
+            Debug.Log("[PC]OnTriggerEnter / Trigger");
+            if (Input.GetKeyDown(KeyCode.F)==true)
+            {
+                Debug.Log("[PC]OnTriggerEnter / keycode F");
+
+            }
+        }
     }
 }
